eb20d7295cd8df5ac42ad21c2a0783d162674d2c
zazalee <zaza.viking@gmail.com>
2026-06-11 14:03:49 +0800

    修改builc-local-image.sh脚本，使用代理

--- build-local-image.sh
+++ build-local-image.sh
@@ -19,7 +19,29 @@ git pull --ff-only --autostash
 
 image_name="${COUNTLY_LOCAL_IMAGE:-soda-countly:local}"
 
+default_proxy="http://127.0.0.1:7890"
+default_no_proxy="localhost,127.0.0.1,::1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,.ivolces.com"
+
+proxy="${HTTPS_PROXY:-${HTTP_PROXY:-}}"
+if [[ -z "$proxy" && "${COUNTLY_BUILD_USE_LOCAL_PROXY:-1}" == "1" ]]; then
+  proxy="$default_proxy"
+fi
+
+no_proxy="${NO_PROXY:-${no_proxy:-$default_no_proxy}}"
+
+build_args=(--pull -f Dockerfile-core -t "$image_name")
+if [[ -n "$proxy" ]]; then
+  echo "Using build proxy: $proxy"
+  build_args+=(--network=host)
+  build_args+=(--build-arg "HTTP_PROXY=$proxy")
+  build_args+=(--build-arg "HTTPS_PROXY=$proxy")
+  build_args+=(--build-arg "http_proxy=$proxy")
+  build_args+=(--build-arg "https_proxy=$proxy")
+  build_args+=(--build-arg "NO_PROXY=$no_proxy")
+  build_args+=(--build-arg "no_proxy=$no_proxy")
+fi
+
 echo "Building local Countly image: $image_name"
-docker build --pull -f Dockerfile-core -t "$image_name" .
+docker build "${build_args[@]}" .
 
 echo "Build finished. Start with: ${compose_cmd[*]} -f docker-compose.yml up -d"
